106e1b06460ca06a1288bcd0114e7746ae05c68f
37009904 <37009904@parisnanterre.fr>
2021-03-31 13:40:59 +0200

    feat: add bcrypt hash password feature to post user.

--- api/authRouter.js
+++ api/authRouter.js
@@ -1,13 +1,29 @@
 var express = require("express");
 var authRouter = express.Router();
+const UserService = require("../services/UserService");
 
-// define the home page route
+// define the authentication routes
+authRouter.post("/registration", async function (req, res) {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    const serviceAnswer = await UserService.postUser(
+      firstName,
+      lastName,
+      email,
+      password
+    );
+    res.json(serviceAnswer);
+  } catch (err) {
+    res.status(err.httpStatusCode).json({ err: err.message });
+  }
+});
+authRouter.get("/registration", function (req, res) {
+  res.send("registration home page");
+});
+
 authRouter.get("/", function (req, res) {
   res.send("authentication home page");
-});
-// define the about route
-authRouter.get("/about", function (req, res) {
-  res.send("About authentication");
 });
 
 module.exports = authRouter;
--- services/UserService.js
+++ services/UserService.js
@@ -1,9 +1,25 @@
 const Object = require("../models/Object");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
 
 class UserService {
   static getUsers() {}
-  static postUser() {}
+  static async postUser(firstName, lastName, email, password) {
+    try {
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const user = { firstName, lastName, email, password: hashedPassword };
+      const postedUser = new User(user);
+      const data = await postedUser.save();
+      return data;
+    } catch (err) {
+      console.log(err);
+      throw new ServiceError(
+        "Erreur, impossible d'ajouter l'utilisateur à la base de donnée.",
+        503
+      );
+    }
+  }
 }
 
 module.exports = UserService;
